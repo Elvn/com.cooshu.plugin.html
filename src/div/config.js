0ce34000fc181c7efafb56c2900dbbb174fcb56f
Elvn <50227733@qq.com>
2020-05-07 14:02:54 +0800

    add kusu-plugin-sdk

--- src/div/config.js
+++
@@ -1,4 +0,0 @@
-export default {
-  pluginName: '@div',
-  pluginType: 'container',
-};
---
+++ src/div/config.ts
@@ -0,0 +1,10 @@
+import { IPluginConfig } from 'kusu-plugin-sdk/src/ILibraryConfig';
+
+const config: IPluginConfig = {
+  pluginName: '@div',
+  pluginType: 'container',
+  attributes: [],
+  styles: []
+};
+
+export default config;
